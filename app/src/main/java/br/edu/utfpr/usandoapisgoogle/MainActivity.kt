eb58931128b443ea0457f2239949adef14e61d70
robisoncrisbrito <robison.brito@gmail.com>
2025-04-16 19:46:57 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/utfpr/usandoapisgoogle/MainActivity.kt
@@ -0,0 +1,68 @@
+package br.edu.utfpr.usandoapisgoogle
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var tvLatitude : TextView
+    private lateinit var tvLongitude : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tvLatitude = findViewById( R.id.tvLatitude )
+        tvLongitude = findViewById( R.id.tvLongitude )
+    }
+
+    fun btVerEnderecoOnClick(view: View) {
+        Thread ( Runnable {
+            val endereco = "https://maps.googleapis.com/maps/api/geocode/xml?latlng=-26.0751195,-53.0613228&key=AIzaSyDsy454kAkXofX828BEMieAQ7EbtpjohZY"
+            val url = URL(endereco)
+            var con = url.openConnection()
+
+            val inputStreamReader = con.getInputStream()
+
+            val entrada = BufferedReader( InputStreamReader( inputStreamReader ) )
+
+            val msgSaida = StringBuilder()
+
+            var linha = entrada.readLine()
+
+            while( linha != null ) {
+                msgSaida.append( linha )
+                linha = entrada.readLine()
+            }
+
+            val formattedAddress = msgSaida.substring(
+                msgSaida.indexOf( "<formatted_address>") + 19,
+                msgSaida.indexOf( "</formatted_address>" )
+            )
+
+            runOnUiThread {
+                val dialog = AlertDialog.Builder(this)
+                dialog.setTitle("Endereço: ")
+                dialog.setMessage(formattedAddress)
+                dialog.setCancelable(false)
+                dialog.setNeutralButton("OK", null)
+                dialog.show()
+            }
+
+
+
+
+        } ).start()
+
+
+    }
+    fun btVerMapaOnClick(view: View) {}
+}
